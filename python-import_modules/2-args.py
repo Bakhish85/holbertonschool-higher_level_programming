9953b9b50ec97c40707a036909747f6eca696ff8
Bakhish85 <umudlu85@gmail.com>
2024-05-09 14:03:15 +0000

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    if len(sys.argv) == 0:
-        print("{} arguments.".format(len(sys.argv)))
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv)-1))
     else:
-        print("{} arguments:".format(len(sys.argv)))
-        for index, element in enumerate(sys.argv):
-            print("{}: {}".format(index+1, element))
+        print("{} arguments:".format(len(sys.argv)-1))
+        for index, element in enumerate(sys.argv[1:]):
+            print("{}: {}".format(index, element))
