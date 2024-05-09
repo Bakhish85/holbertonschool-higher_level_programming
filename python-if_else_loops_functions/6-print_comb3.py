f37b76a764a9016d4cc400584f2a0abad5e0f568
Bakhish85 <umudlu85@gmail.com>
2024-05-09 07:45:12 +0000

    pycodestyle error

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for number1 in range(0,9):
-    for number2 in range(1,10):
-        if number1 == 8 and number2 ==9:
+for number1 in range(0, 9):
+    for number2 in range(1, 10):
+        if number1 == 8 and number2 == 9:
             print("{}{}".format(number1, number2))
         else:
             if number1 != number2 and number1 < number2:
-                print("{}{}, ".format(number1, number2), end = "")
-        
+                print("{}{}, ".format(number1, number2), end="")
