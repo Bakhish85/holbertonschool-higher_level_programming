935cc9e60e29a32f5552ac8e037bfefdc37b9cc6
Bakhish85 <umudlu85@gmail.com>
2024-05-09 06:26:38 +0000

    print comb

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+result = ""
 for number in range(0, 100):
     if number != 99:
         if number >= 0 and number <= 9:
-            print("0{}, ".format(number), end = "")
+            result += "0{}, ".format(number)
         else:
-            print("{}, ".format(number), end = "")
+            result += "{}, ".format(number)
     else:
-        print("{}".format(number))
+        result += "{}".format(number)
+print(result)
