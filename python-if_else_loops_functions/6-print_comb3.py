393f178d36a00c3ba50df2a9141d5d29c1858f90
Bakhish85 <umudlu85@gmail.com>
2024-05-09 06:59:11 +0000

    00-89

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+for number1 in range(0,9):
+    for number2 in range(1,10):
+        if number1 == 8 and number2 ==9:
+            print("{}{}".format(number1, number2))
+        else:
+            if number1 != number2 and number1 < number2:
+                print("{}{}, ".format(number1, number2), end = "")
+        
