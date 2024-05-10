2d71d2af40e02df79d76e67985742e7cc8019e75
Bakhish85 <umudlu85@gmail.com>
2024-05-10 11:20:31 +0000

    fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == '__main__':
     result = 0
     if len(sys.argv) > 1:
-        for number in sys.argv:
+        for number in sys.argv[1:]:
             result += int(number)
         print("{}".format(result))
