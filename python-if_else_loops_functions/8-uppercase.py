bec24f03a1028ee04b37cda997b269641ba628e2
Bakhish85 <umudlu85@gmail.com>
2024-05-13 16:00:57 +0000

    fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for index, char in enumerate(str):
-        if index == len(str) - 1:
-            if ord(char) >= 97:
-                char = chr(ord(char) - 32)
-            print("{}".format(char))
-        else:
-            if ord(char) >= 97:
-                char = chr(ord(char) - 32)
-            print("{}".format(char), end="")
+    new_str = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        new_str += char
+    print("{}".format(new_str))
