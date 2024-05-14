918f15056616074b31b93a60d523e8b3e2ca9d12
Bakhish85 <umudlu85@gmail.com>
2024-05-14 05:34:40 +0000

    fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+string = ""
 char_list = []
 for ascii_number in range(97,123):
     if ascii_number % 2 == 1:
         ascii_number -= 32
-    char_list.append(chr(ascii_number))
+    string += chr(ascii_number)
+char_list.extend(string)
 char_list.reverse()
-char_string = "".join(char_list)
-print("{}".format(char_string), end="")
+new_string = "".join(char_list)
+print("{}".format(new_string), end="")
