3b3511c7e502171a5a9ee975f6f1d1246c3842c7
Bakhish85 <umudlu85@gmail.com>
2024-05-14 09:33:16 +0000

    fix import

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, mul, div, sub
 
 
 def main():
     operator_list = ['+', '-', '*', '/']
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         print("1")
         exit()
     else:
-        if sys.argv[2] not in operator_list:
+        if argv[2] not in operator_list:
             print("Unknown operator. Available operators: +, -, * and /")
             print("1")
             exit()
         else:
-            if sys.argv[2] == '+':
-                print("{:d} {} {:d} = {:d}". format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+            if argv[2] == '+':
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), add(int(argv[1]), int(argv[3]))))
                 print("0")
                 exit()
-            elif sys.argv[2] == '-':
-                print("{:d} {} {:d} = {:d}". format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+            elif argv[2] == '-':
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), sub(int(argv[1]), int(argv[3]))))
                 print("0")
                 exit()
-            elif sys.argv[2] == '*':
-                print("{:d} {} {:d} = {:d}". format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+            elif argv[2] == '*':
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), mul(int(argv[1]), int(argv[3]))))
                 print("0")
                 exit()
-            elif sys.argv[2] == '/':
-                print("{:d} {} {:d} = {:d}". format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
+            elif argv[2] == '/':
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), div(int(argv[1]), int(argv[3]))))
 
 if __name__ == '__main__':
     main()
