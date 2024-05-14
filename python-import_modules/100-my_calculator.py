7531132d23534b6ae8dde9e9ca6aebddc5b3a12e
Bakhish85 <umudlu85@gmail.com>
2024-05-14 10:19:34 +0000

    fix pycodestyle

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -11,21 +11,37 @@ def main():
     else:
         if argv[2] not in operator_list:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(0)
+            exit(1)
         else:
             if argv[2] == '+':
-                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), add(int(argv[1]), int(argv[3]))))
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]),
+                                                    argv[2],
+                                                    int(argv[3]),
+                                                    add(int(argv[1]),
+                                                        int(argv[3]))))
                 exit(0)
             elif argv[2] == '-':
-                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]),
+                                                    argv[2],
+                                                    int(argv[3]),
+                                                    sub(int(argv[1]),
+                                                        int(argv[3]))))
                 exit(0)
             elif argv[2] == '*':
-                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]),
+                                                    argv[2],
+                                                    int(argv[3]),
+                                                    mul(int(argv[1]),
+                                                        int(argv[3]))))
                 exit(0)
             elif argv[2] == '/':
-                print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), div(int(argv[1]), int(argv[3]))))
+                print("{:d} {} {:d} = {:d}". format(int(argv[1]),
+                                                    argv[2],
+                                                    int(argv[3]),
+                                                    div(int(argv[1]),
+                                                        int(argv[3]))))
                 exit(0)
+
 
 if __name__ == '__main__':
     main()
-
