b700673f367e6ca472a3e9ca95068ab809c9895e
Bakhish85 <umudlu85@gmail.com>
2024-05-14 09:56:02 +0000

    fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sys import argv
+from sys import argv, exit
 from calculator_1 import add, mul, div, sub
 
 
@@ -7,28 +7,24 @@ def main():
     operator_list = ['+', '-', '*', '/']
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print("1")
-        exit()
+        exit(1)
     else:
         if argv[2] not in operator_list:
             print("Unknown operator. Available operators: +, -, * and /")
-            print("1")
-            exit()
+            exit(0)
         else:
             if argv[2] == '+':
                 print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), add(int(argv[1]), int(argv[3]))))
-                print("0")
-                exit()
+                exit(0)
             elif argv[2] == '-':
                 print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), sub(int(argv[1]), int(argv[3]))))
-                print("0")
-                exit()
+                exit(0)
             elif argv[2] == '*':
                 print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), mul(int(argv[1]), int(argv[3]))))
-                print("0")
-                exit()
+                exit(0)
             elif argv[2] == '/':
                 print("{:d} {} {:d} = {:d}". format(int(argv[1]), argv[2], int(argv[3]), div(int(argv[1]), int(argv[3]))))
+                exit(0)
 
 if __name__ == '__main__':
     main()
