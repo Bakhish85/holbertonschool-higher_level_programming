2d2be1db8b3ea6d54dceb51fd2cdf7cd78f14e29
Bakhish85 <umudlu85@gmail.com>
2024-05-20 09:21:11 +0000

    pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """
-    Function returns a tuple with the length of a string and its first character.
-    
+    Function returns a tuple with the length of a string
+    and its first character.
     Args:
         sentence (str): The input sentence.
-    
     Returns:
         tuple: A tuple containing two elements.
             - First element: Length of the string
             - Second element: First character of the string.
-            If the input sentence is an empty string, the second element of the tuple will be None.
+            If the input sentence is an empty string,
+            the second element of the tuple will be None.
     """
     new_list = []
     new_list.append(len(sentence))
@@ -20,4 +20,3 @@ def multiple_returns(sentence):
         new_list.append(sentence[0])
     new_tuple = tuple(new_list)
     return new_tuple
-
