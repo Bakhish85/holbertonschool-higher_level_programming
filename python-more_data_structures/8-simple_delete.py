6556c573d914c2c145f873c1931c02427a21afb5
Bakhish85 <umudlu85@gmail.com>
2024-05-20 14:57:42 +0000

    delete dict

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+    The function deletes a key in a dictionary.
+    Args:
+        a_dictionary (dict):    The input dictionary
+        key (str):      The key to be deleted.
+    Returns:
+        (dict): Returns updated dictionary
+        with deleted key and its value.
+        - If a key does not exist,
+        the dictionary will not change.
+    """
+    key_del = a_dictionary.get(key)
+    if key_del != None:
+        del a_dictionary[key]
+    return a_dictionary
