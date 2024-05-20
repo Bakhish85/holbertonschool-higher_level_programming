ce78161413b7986f23e2b69d53dce90b98a0da3c
Bakhish85 <umudlu85@gmail.com>
2024-05-20 14:18:32 +0000

    update dict

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    The function replaces or adds key/value in a dictionary.
+    Args:
+        a_dictionary (dict):    The input dictionary
+        key (str):      The key to be added or updated.
+        value (Any):    The value to be added or updated.
+    Returns:
+        dict:   Returns updated dictionary.
+            - If a key exists in the dictionary,
+            the value will be replaced.
+            - If a key does not exist in the dictionary
+            it will be created
+    """
+    a_dictionary[key] = values
+    return a_dictionary
