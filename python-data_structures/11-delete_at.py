e3ac9f3100680069cfdeee215b4425245fcce468
Bakhish85 <umudlu85@gmail.com>
2024-05-20 10:10:21 +0000

    delete item

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """
+    Function deletes the item at a specific position in a list.
+    Args:
+        my_list (list): The input list
+        idx (int):      The specific position in the list to be deleted
+    Returns:
+        list:           The list with deleted item.
+                        If idx is negative or out of range,
+                        nothing change (returns the same list).
+    """
+    if idx < 0 or idx >= len(my_list) - 1:
+        return my_list
+    my_list.remove(my_list[idx])
+    return my_list
