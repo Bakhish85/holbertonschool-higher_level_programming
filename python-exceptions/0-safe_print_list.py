27a875c7d6468ca9396554416a823e08d72e3a68
Bakhish85 <umudlu85@gmail.com>
2024-05-22 08:17:11 +0000

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,14 +3,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for number in my_list:
-            count += 1
-            if count <= x:
+            if count < x:
+                count += 1
                 print("{}".format(number), end = "")
         print("")
     except:
         pass
     finally:
-        if x <= len(my_list):
-            return x
-        else:
-            return len(my_list) 
+        return count 
