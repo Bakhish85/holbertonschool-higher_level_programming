df3e09361c582daaa80b23adf35b267218c9ce4b
Bakhish85 <umudlu85@gmail.com>
2024-05-22 08:19:06 +0000

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
         for number in my_list:
             if count < x:
                 count += 1
-                print("{}".format(number), end = "")
+                print("{}".format(number), end="")
         print("")
     except:
-        pass
+        print("Unknown error")
     finally:
-        return count 
+        return count
