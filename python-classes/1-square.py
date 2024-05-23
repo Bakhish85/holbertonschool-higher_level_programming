8b0f375b4ddde3f85cbc89592100c1f733c41c98
Bakhish85 <umudlu85@gmail.com>
2024-05-23 09:40:12 +0000

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module: square
+
+This module defines a simple Square class for representing and manipulating square objects.
+"""
+class Square:
+    """
+    A class representing a square.
+    This class provides a simple way to create
+    and manipulate square objects.Each square
+    is defined by its size.
+
+    Attributes:
+        __size (float or int):  The size of each side of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (float or int): The size of each side of the square.
+        """
+        self.__size = size
