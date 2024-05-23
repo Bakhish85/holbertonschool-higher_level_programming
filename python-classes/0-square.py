09f7a14238204319e26dea59b0e3e6d1a32ef0b3
Bakhish85 <umudlu85@gmail.com>
2024-05-23 08:37:40 +0000

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,4 +3,9 @@
 This class does not do anything for the moment.
 """
 class Square:
+    """A class representing a square.
+    This class does not have any attributes or
+    methods defined yet. It serves as a placeholder
+    for defining square objects with speceific properties.
+    """
     pass
