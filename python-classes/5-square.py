e0ad06276e400b414c5558931b20f6eb840363b1
Bakhish85 <umudlu85@gmail.com>
2024-05-23 12:51:33 +0000

    print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module: square
+
+This module defines a simple Square class for representing
+and manipulating square objects.
+"""
+
+
+# Class definition
+class Square:
+    """
+    A class representing a square.
+
+    This class provides a simple way to create and manipulate
+    square objects. Each sqaure is defined by its size.
+
+    Attributes:
+        __size (int):   The size of each side of the square.
+
+    Methods:
+        __init__(size=0):   Initializes a enw Square instance with
+                            the given size.
+        area():             Calculates the area of the square
+        my_print():         Prints a visual representation of the
+                            e using '#' characters.
+        size:               Property to get or set the size of the square.
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
