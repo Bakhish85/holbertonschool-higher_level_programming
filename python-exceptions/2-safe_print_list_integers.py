5fd69010e07876bd135e7fa121ab0cc69762eb39
Bakhish85 <umudlu85@gmail.com>
2024-05-23 06:23:35 +0000

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for number in my_list:
         try:
-            print("{:d}".format(number))
+            print("{:d}".format(number), end="")
             count += 1
             if count == x:
                 break
