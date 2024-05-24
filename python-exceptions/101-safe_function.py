ced69dd020ff22cba972b72493dc6557e034eaf3
Bakhish85 <umudlu85@gmail.com>
2024-05-24 08:36:21 +0000

    fix

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        fct(*args)
+        return fct(*args)
     except ZeroDivisionError as e:
         sys.stderr.write(f"Exception: {e}\n")
         return None
