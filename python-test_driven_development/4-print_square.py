1ba67c33628976017e408b4b1def46ca8f5ec394
Bakhish85 <umudlu85@gmail.com>
2024-05-27 12:37:03 +0000

    fix

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -23,7 +23,7 @@ def print_square(size):
     """
     if type(size) not in [int]:
         raise TypeError("size must be an integer")
-    elif size > 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
     for _ in range(size):
         print('#' * size)
