3a9a3eceb6b804145c3c6b20dc378ce9bf30dc91
Bakhish85 <umudlu85@gmail.com>
2024-05-27 07:05:09 +0000

    add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module: arithmetic_operations
+
+This module provides arithmetic operations for integers.
+
+Functions:
+    - add_integer(a, b=98): Adds two integers together.
+"""
+def add_integer(a, b=98):
+    """
+    Adds two integers together.
+
+    Parameters:
+    - a(int or float):    The first integer to be added.
+    - b(int or float):    The second integer to be added.
+                            Default is 98.
+
+    Returns:
+    int:    The sum of two integers.
+
+    Raises:
+    - TypeError:    If `a` or `b` is not an integer.
+    """
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+# This directory contains task about python-test driven development
