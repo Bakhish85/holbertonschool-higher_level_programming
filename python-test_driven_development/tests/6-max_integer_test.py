6b6a1f84dc5cf180ec97cc3a7e03fb412ed6cfb7
Bakhish85 <umudlu85@gmail.com>
2024-05-29 09:42:58 +0000

    fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,7 @@
 Unittest for max_integer([..])
 """
 import unittest
-max_integer = __import__('test2').max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 def test_result(self):
