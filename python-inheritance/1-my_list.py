e820099bf8296cf12be340320e55a15286842c8c
Bakhish85 <umudlu85@gmail.com>
2024-05-29 14:48:23 +0000

    sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-my_list module
+
+This module defines a custom list class called MyList.
+
+Classes:
+    MyList: A custom list class inheriting
+    from the built-in list class.
+"""
+
+
+class MyList(list):
+    """
+    A custom list class inheriting from the
+    built-in list class.
+
+    Methods:
+        print_sorted(self): Prints the elements
+        of the list in the sorted order.
+    """
+    new_list = sorted(self)
+    print(new_list)
