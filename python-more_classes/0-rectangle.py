cffbf72c7b89e2c2d691906ab8f3d722fda32730
Bakhish85 <umudlu85@gmail.com>
2024-05-29 10:33:12 +0000

    fix

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+Module: rectangle
+
+This module defines a basic Rectangle class.
+"""
 
 
 class Rectangle:
-    
-    def __init__(self):
-        pass
+    """
+    A basic rectangle class.
+
+    This class does not have any attributes or methods defined.
+    """
+    pass
