8a782ec2e914c023f91a4348a8f67ca53ef5e5ca
Bakhish85 <umudlu85@gmail.com>
2024-05-29 08:06:41 +0000

    text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,40 @@
+"""
+Module: text_indentation
+
+This module provides a function for formatting text
+with indentation based on specific punctuation marks.
+
+Functions:
+    - text_indentation(text):   Formats the given text
+    with indentation based on specific punctuation marks.
+"""
+
+
+def text_indentation(text):
+    """
+    Formats the given text with indentation based on specific
+    punctuation marks.
+
+    Parameters:
+        text(str):  The text to be formatted
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if type(text) not in [str]:
+        raise TypeError("text must be a string")
+    mark_list = ['.', '?', ':']
+    new_text = ""
+    idx = 0
+    while idx < len(text):
+        if text[idx-1] in mark_list and text[idx] == ' ':
+            idx += 1
+            continue
+        elif text[idx] in mark_list:
+            new_text += text[idx]
+            new_text += '\n\n'
+            idx += 1
+            continue
+        new_text += text[idx]
+        idx += 1
+    print(new_text, end="")
