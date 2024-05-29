275af3a8221e9667916772d60693a2ac7b0de5ae
Bakhish85 <umudlu85@gmail.com>
2024-05-29 11:19:37 +0000

    fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
     def width(self, value):
         if type(value) not in [float, int]:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -59,8 +59,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(height) not in [float, int]:
+        if type(value) not in [float, int]:
             raise TypeError("height must be an integer")
-        elif height < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
