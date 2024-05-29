aae4bf46fa137b95c250e46d405c966ef8f743ac
Bakhish85 <umudlu85@gmail.com>
2024-05-29 09:56:27 +0000

    fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,10 +9,10 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_result(self):
         # Test the function result with correct type of values.
-        self.assertAlmostEqual(max_integer([1, 2, 3, 4], 4))
-        self.assertAlmostEqual(max_integer([4, 2, 3, -4], 4))
-        self.assertAlmostEqual(max_integer([1.2, 12.3, 4, -5.7], 12.3))
-        self.assertAlmostEqual(max_integer([], None)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([4, 2, 3, -4]), 4)
+        self.assertAlmostEqual(max_integer([1.2, 12.3, 4, -5.7]), 12.3)
+        self.assertAlmostEqual(max_integer([]), None)
 
     def test_values(self):
         # Test if the argument of function is list.
