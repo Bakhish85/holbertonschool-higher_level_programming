3417a4521e535c7144e76faf35c339b22f2fa8ed
Bakhish85 <umudlu85@gmail.com>
2024-05-30 07:42:25 +0000

    empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+base_geometry module
+
+This module defines empty class named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    This class is empty.
+    """
+    pass
