1d14b703853368861359d3f85e06258e0079411f
Bakhish85 <umudlu85@gmail.com>
2024-05-31 06:20:38 +0000

    fix

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+Module: shapes.py
+
+This module defines classes representing geometric shapes.
+
+Classes:
+        Rectangle: A class representing a rectangle shape.
+        Square: A class representing a square shape, inheriting
+                from Rectangle.
+"""
+
+
 Rectangle = __import__('9-rectangle.py').Rectangle
 Square = __import__('10-square.py').Square
+
+
+class Sqaure(Rectangle):
 """
 Square class represents a square shape and inherits
 from Rectangle.
@@ -12,8 +27,5 @@ Methods:
     __str__():  Returns a string representation of the Square
                 object.
 """
-
-
-class Square(Rectangle):
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"
