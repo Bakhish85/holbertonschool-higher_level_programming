5cdad45b92bf4051c9e99172f9a0ac7ef9feaf3c
Bakhish85 <umudlu85@gmail.com>
2024-05-31 10:02:19 +0000

    verbolist

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, item=-1):
+        print("Popped [{}] from the list.".format(self[item]))
+        super().pop()
+
