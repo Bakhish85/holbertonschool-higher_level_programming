69dbf21f77cd656b90c51359a4eb2d5542fe7633
Bakhish85 <umudlu85@gmail.com>
2024-05-31 06:17:18 +0000

    fix

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle.py').Rectangle
 Square = __import__('10-square.py').Square
+"""
+Square class represents a square shape and inherits
+from Rectangle.
+
+Attributes:
+    __size (int): The size of the square.
+
+Methods:
+    __str__():  Returns a string representation of the Square
+                object.
+"""
 
 
 class Square(Rectangle):
