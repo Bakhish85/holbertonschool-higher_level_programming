08f86c6934b912d76f46c5fc9801d8750f362ae3
Bakhish85 <umudlu85@gmail.com>
2024-05-31 11:36:39 +0000

    fix

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,14 +1,11 @@
 class CountedIterator:
-    def __init__(self, iter_object, counter=0):
-        self.iter_object = iter_object
-        self.counter = counter
+    def __init__(self, iter_object):
+        self.iter_object = iter(iter_object)
+        self.counter = 0
 
     def get_count(self):
         return self.counter
 
     def __next__(self):
         self.counter += 1
-        if self.counter <= len(self.iter_object):
-            return self.iter_object[self.counter - 1]
-        else:
-            raise StopIteration
+        return next(self.iter_object)
