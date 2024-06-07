f82eb6d5d86bb699c2fecd0cb85db058c6a2c136
Bakhish85 <umudlu85@gmail.com>
2024-06-07 10:43:43 +0000

    append and count

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module provides functions for reading from and writing to files.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Append the specified text to the given file
+    and return the number of characters added.
+
+    Args:
+        filename (str): The path to the file to append the text to.
+                        If not provided, an empty string is assumed.
+        text (str):     The text to be appended to the file.
+
+    Returns:
+        int:            The number of characters added to the file.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
