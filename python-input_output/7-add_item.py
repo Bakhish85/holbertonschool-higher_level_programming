02f0fdbb6248c36fe8236238ef0da63050cf37b0
Bakhish85 <umudlu85@gmail.com>
2024-06-08 10:39:16 +0000

    sys argv

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script adds all its arguments to a Python list
+and then saves them to a file using the provided functions
+save_to_json_file and load_from_json_file.
+"""
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+def main():
+    """
+    Add command-line arguments to a Python list
+    and save them to a file.
+    If the file 'add_item.json' exists,
+    load its contents into a list.
+    If the file doesn't exist, initialize an empty list.
+    Append command-line arguments to the list.
+    Save the updated list to the file 'add_item.json' using JSON serialization.
+
+    Parameters: none
+
+    Returns: none
+    """        
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, "add_item.json")
+
+if __name__ == '__main__':
+    main()
