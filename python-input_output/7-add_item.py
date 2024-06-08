50c4ec8cfe9e03af4e68f1c0afa8998f94aae915
Bakhish85 <umudlu85@gmail.com>
2024-06-08 10:40:11 +0000

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ and then saves them to a file using the provided functions
 save_to_json_file and load_from_json_file.
 """
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
 def main():
