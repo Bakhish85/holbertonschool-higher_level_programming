1f94fac6d79dd8d401f627919b06670ec930aa99
Bakhish85 <umudlu85@gmail.com>
2024-06-29 05:40:33 +0000

    fix

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,12 @@
--- Creates the database hbtn_0d_2 if it does not already exist
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-
--- Creates the user user_0d_2 if it does not already exist, identified by password user_0d_2_pwd
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grants SELECT privilege on all tables in hbtn_0d_2 database to user_0d_2
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Flush privileges to apply the changes
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+	IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+	IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+	ON `hbtn_0d_2`.*
+	TO 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
-
