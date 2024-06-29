39ff5197f39ddaaf1a271f8ddf155172bc91cca2
Bakhish85 <umudlu85@gmail.com>
2024-06-29 05:35:36 +0000

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
 -- Creates the user user_0d_1 with all privileges.
-CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+/* Grant all privileges to user_0d_1 */
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
